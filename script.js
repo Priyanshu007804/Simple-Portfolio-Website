d8c804abd628e7dfca7678eb44920a3a2d6eb497
Priyanshu007804 <binoy12131415@gmail.com>
2025-09-06 01:35:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
-      window.addEventListener('scroll', function() {
+document.addEventListener('DOMContentLoaded', function() {
+      
+     
+      const animatableElements = document.querySelectorAll('.animate');
+      
+     
+      const observerOptions = {
+       
+        root: null,
+        
+        
+        rootMargin: '150px 0px',
+        
+       
+        threshold: 0.3
+      };
+      
+      const observer = new IntersectionObserver((entries, observer) => {
+        
+        entries.forEach(entry => {
+          
+          if (entry.isIntersecting) {
+            
+            entry.target.classList.add('visible');
+            
+       
+            observer.unobserve(entry.target);
+          }
+        });
+      }, observerOptions);
+      
+      animatableElements.forEach(element => {
+        observer.observe(element);
+      });
+
+       window.addEventListener('scroll', function() {
         const navbar = document.querySelector('.navbar');
-        if (window.scrollY > 100) {
+        if (window.scrollY > 10) {
           navbar.classList.add('scrolled');
         } else {
           navbar.classList.remove('scrolled');
         }
       });
       
- window.addEventListener('load', function() {
+     window.addEventListener('load', function() {
         const text = " Hi! I am Priyanshu Singha Roy";
         const heroText = document.getElementById('hero-text');
         if (heroText) {
@@ -24,21 +59,13 @@
         }
       });
 
-      var tablinks= document.getElementsByClassName("tab-links");
-            var tabcontents= document.getElementsByClassName("tab-contents");
-            function opentab(tabname){
-                for(tablink of tablinks){
-                    tablink.classList.remove("active-link");
-                }
-                for(tabcontent of tabcontents){
-                    tabcontent.classList.remove("active-tab");
-                }
-                event.currentTarget.classList.add("active-link");
-                document.getElementById(tabname).classList.add("active-tab");
-            }
-
-      function showEcellWeb(){
-        alert('This Website will be availabe soon.');
-      }
-
-      
+       document.querySelector('.animate-name').addEventListener('mouseenter', () => {
+            document.querySelector('.animate-name').style.animation = 
+                'gradientShift 3s ease infinite, pulse 0.5s ease infinite';
+        });
+        
+        document.querySelector('.animate-name').addEventListener('mouseleave', () => {
+            document.querySelector('.animate-name').style.animation = 
+                'gradientShift 6s ease infinite, pulse 2s ease infinite';
+        });
+       });
